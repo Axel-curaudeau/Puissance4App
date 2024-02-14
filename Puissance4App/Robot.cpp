4ed74d0c388240d1e3ac3542700594a74df9107d
Axel-curaudeau <axelcuraudeau@gmail.com>
2024-02-14 10:44:34 +0100

    added search function for finding robot

--- Puissance4App/Robot.cpp
+++ Puissance4App/Robot.cpp
@@ -1,9 +1,15 @@
 #include "Robot.hpp"
+#include <iostream>
 
 Robot::Robot()
 {
 	char dobotId[64];
 	int dobotSearchCount = SearchDobot(dobotId, 64);
 
+	std::cout << "Dobot count: " << dobotSearchCount << std::endl;
+
+	for (int i = 0; i < dobotSearchCount; i++) {
+		std::cout << "Dobot ID: " << dobotId << std::endl;
+	}
 
 }
