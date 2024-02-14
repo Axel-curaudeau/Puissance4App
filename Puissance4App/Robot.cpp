4fd0144f907af15d9f1bec87bdc08b49293c219c
demorobot <demorobot@EPU-2019000516>
2024-02-14 11:01:39 +0100

    Added connection to dobot

--- Puissance4App/Robot.cpp
+++ Puissance4App/Robot.cpp
@@ -3,13 +3,25 @@
 
 Robot::Robot()
 {
-	char dobotId[64];
-	int dobotSearchCount = SearchDobot(dobotId, 64);
+	char dobotPortName[64];
+	int dobotSearchCount = SearchDobot(dobotPortName, 64);
 
 	std::cout << "Dobot count: " << dobotSearchCount << std::endl;
 
-	for (int i = 0; i < dobotSearchCount; i++) {
-		std::cout << "Dobot ID: " << dobotId << std::endl;
+	if (dobotSearchCount == 0) {
+		std::cerr << "No dobot found" << std::endl;
+		return;
+	}
+	else if (dobotSearchCount > 1) {
+		std::cerr << "Multiple dobot found" << std::endl;
+		return;
 	}
 
+	int dobotId;
+	if (ConnectDobot(dobotPortName, 115200, nullptr, nullptr, &dobotId) != DobotConnect_NoError) {
+		std::cerr << "Failed to connect to dobot" << std::endl;
+		return;
+	}
+
+	std::cout << "Connected to dobot" << std::endl;
 }
